dab9d11124bcf943eea38526094d65b522db308d
kumusan <ryogi0200@gmail.com>
2020-10-09 14:25:35 +0900

    🤞pipeline

--- pipeline/src/main.rs
+++ pipeline/src/main.rs
@@ -1,17 +1,17 @@
+use std::iter;
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
-    window::{Window, WindowBuilder}
+    window::{Window, WindowBuilder},
 };
-use std::iter;
 
 struct State {
-    surface: wgpu::Surface,             // 表面
-    device: wgpu::Device,               // デバイス
-    queue: wgpu::Queue,                 // キュー
-    sc_desc: wgpu::SwapChainDescriptor,  
-    swap_chain: wgpu::SwapChain,         
-    size: winit::dpi::PhysicalSize<u32>,// サイズ 
+    surface: wgpu::Surface, // 表面
+    device: wgpu::Device,   // デバイス
+    queue: wgpu::Queue,     // キュー
+    sc_desc: wgpu::SwapChainDescriptor,
+    swap_chain: wgpu::SwapChain,
+    size: winit::dpi::PhysicalSize<u32>, // サイズ
     clear_color: wgpu::Color,
     render_pipeline: wgpu::RenderPipeline,
 }
@@ -20,25 +20,31 @@ impl State {
     async fn new(window: &Window) -> Self {
         let size = window.inner_size();
 
-        // adapter, surfaceの作成 backend指定 
+        // adapter, surfaceの作成 backend指定
         let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
         // make surface
         let surface = unsafe { instance.create_surface(window) };
         // enumerate adapter
-        let adapter = instance.request_adapter(&wgpu::RequestAdapterOptions {
-            power_preference: wgpu::PowerPreference::Default, // power preference
-            compatible_surface: Some(&surface), // 互換性
-        }).await.unwrap();
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::Default, // power preference
+                compatible_surface: Some(&surface),               // 互換性
+            })
+            .await
+            .unwrap();
 
-        let (device, queue) = adapter.request_device(
-            // device
-            &wgpu::DeviceDescriptor {
-                features: wgpu::Features::empty(),
-                limits: wgpu::Limits::default(),
-                shader_validation: true,
-            },
-            None,
-        ).await.unwrap();
+        let (device, queue) = adapter
+            .request_device(
+                // device
+                &wgpu::DeviceDescriptor {
+                    features: wgpu::Features::empty(),
+                    limits: wgpu::Limits::default(),
+                    shader_validation: true,
+                },
+                None,
+            )
+            .await
+            .unwrap();
 
         let sc_desc = wgpu::SwapChainDescriptor {
             usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
@@ -47,6 +53,87 @@ impl State {
             height: size.height,
             present_mode: wgpu::PresentMode::Fifo,
         };
+
+        let vs_src = include_str!("shader.vert");
+        let fs_src = include_str!("shader.frag");
+        let mut compiler = shaderc::Compiler::new().unwrap();
+        let vs_spirv = compiler
+            .compile_into_spirv(
+                vs_src,
+                shaderc::ShaderKind::Vertex,
+                "shader.vert",
+                "main",
+                None,
+            )
+            .unwrap();
+        let fs_spirv = compiler
+            .compile_into_spirv(
+                fs_src,
+                shaderc::ShaderKind::Fragment,
+                "shader.frag",
+                "main",
+                None,
+            )
+            .unwrap();
+
+        // SPIR-V or WGSL
+        let vs_module =
+            device.create_shader_module(wgpu::util::make_spirv(&vs_spirv.as_binary_u8()));
+        let fs_module =
+            device.create_shader_module(wgpu::util::make_spirv(&fs_spirv.as_binary_u8()));
+
+        // PipelineLayout
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
+
+        // RenderPipeline グラフィック・パイプライン、バインディング、頂点バッファ、ターゲット
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            // デバックラベル
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            // 頂点ステージ、エントリポイント
+            vertex_stage: wgpu::ProgrammableStageDescriptor {
+                module: &vs_module,
+                entry_point: "main",
+            },
+            // フラグメントステージ、エントリポイント
+            fragment_stage: Some(wgpu::ProgrammableStageDescriptor {
+                module: &fs_module,
+                entry_point: "main",
+            }),
+            // ラスタライズプロセス
+            rasterization_state: Some(wgpu::RasterizationStateDescriptor {
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: wgpu::CullMode::Back,
+                depth_bias: 0,
+                depth_bias_slope_scale: 0.0,
+                depth_bias_clamp: 0.0,
+                clamp_depth: false,
+            }),
+            // 出力ターゲット色
+            color_states: &[wgpu::ColorStateDescriptor {
+                format: sc_desc.format,
+                color_blend: wgpu::BlendDescriptor::REPLACE,
+                alpha_blend: wgpu::BlendDescriptor::REPLACE,
+                write_mask: wgpu::ColorWrite::ALL,
+            }],
+            // 頂点解釈に使用されるプリミティブトポロジ
+            primitive_topology: wgpu::PrimitiveTopology::TriangleList,
+            //
+            depth_stencil_state: None,
+            // 頂点入力状態
+            vertex_state: wgpu::VertexStateDescriptor {
+                index_format: wgpu::IndexFormat::Uint16,
+                vertex_buffers: &[],
+            },
+            sample_count: 1,
+            sample_mask: !0,
+            alpha_to_coverage_enabled: false,
+        });
 
         let swap_chain = device.create_swap_chain(&surface, &sc_desc);
 
@@ -60,6 +147,7 @@ impl State {
             swap_chain,
             size,
             clear_color,
+            render_pipeline,
         }
     }
 
@@ -72,7 +160,7 @@ impl State {
 
     fn input(&mut self, event: &WindowEvent) -> bool {
         match event {
-            WindowEvent::CursorMoved{ position, .. } => {
+            WindowEvent::CursorMoved { position, .. } => {
                 self.clear_color = wgpu::Color {
                     r: position.x as f64 / self.size.width as f64,
                     g: position.y as f64 / self.size.height as f64,
@@ -81,39 +169,42 @@ impl State {
                 };
                 true
             }
-            _ => false
+            _ => false,
         }
     }
 
-    fn update(&mut self) {
-
-    }
+    fn update(&mut self) {}
 
     fn render(&mut self) {
         // return texture(frame)
-        let frame = self.swap_chain.get_current_frame()
-                        .expect("Timeout getting texture")
-                        .output;
+        let frame = self
+            .swap_chain
+            .get_current_frame()
+            .expect("Timeout getting texture")
+            .output;
         // GPU operations encode
-        let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
-            label: Some("Render Encoder"),
-        });
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
 
         {
             // render pass の記録開始
-            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                color_attachments: &[
-                    wgpu::RenderPassColorAttachmentDescriptor {
-                        attachment: &frame.view,
-                        resolve_target: None,
-                        ops: wgpu::Operations {
-                            load: wgpu::LoadOp::Clear(self.clear_color),
-                            store: true,
-                        }
-                    }
-                ],
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
+                    attachment: &frame.view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(self.clear_color),
+                        store: true,
+                    },
+                }],
                 depth_stencil_attachment: None,
             });
+
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.draw(0..3, 0..1);
         }
 
         self.queue.submit(std::iter::once(encoder.finish()));
@@ -130,7 +221,10 @@ fn main() {
     let mut state = block_on(State::new(&window));
 
     event_loop.run(move |event, _, control_flow| match event {
-        Event::WindowEvent { ref event, window_id, } if window_id == window.id() => {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() => {
             if !state.input(event) {
                 match event {
                     WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
@@ -151,7 +245,7 @@ fn main() {
                     _ => {}
                 }
             }
-        },
+        }
         Event::RedrawRequested(_) => {
             state.update();
             state.render();
@@ -162,4 +256,3 @@ fn main() {
         _ => {}
     });
 }
-
